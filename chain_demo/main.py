24fd6203aa8119b503a7a343bf707bf73034058a
Santiago Lizarazu <santiago.lizarazu@globant.com>
2022-04-06 22:36:38 -0500

    refactor: Use Application to bundle middleware

--- chain_demo/main.py
+++ chain_demo/main.py
@@ -2,40 +2,47 @@ from http.server import CGIHTTPRequestHandler
 from socketserver import TCPServer
 
 from endpoints import hello, save
-from middleware import (AuthHandler, DummyHandler, MiddlewareHandler,
-                        RouterHandler)
+from middleware import (AuthHandler, CacheHandler, DummyHandler,
+                        MiddlewareHandler, RouterHandler)
 
 
-def create_middleware(router: RouterHandler) -> MiddlewareHandler:
-    dummy_handler = DummyHandler()
-    auth_handler = AuthHandler()
-
-    dummy_handler.then(auth_handler)
-    auth_handler.then(router)
-
-    return dummy_handler
-
-
-class Handler(CGIHTTPRequestHandler):
-    def __init__(self, *args, **kwargs) -> None:
+class Application:
+    def __init__(self) -> None:
         router = RouterHandler()
         self._register_routes(router)
-        self.middleware = create_middleware(router)
-        super().__init__(*args, **kwargs)
+        self.middleware = self._create_middleware(router)
 
     def _register_routes(self, router: RouterHandler):
         router.add_endpoint("GET", "/hello", hello)
         router.add_endpoint("POST", "/save", save)
 
-    def do_GET(self) -> None:
-        self.middleware.handle(self)
+    def _create_middleware(self, router: RouterHandler) -> MiddlewareHandler:
+        dummy_handler = DummyHandler()
+        auth_handler = AuthHandler()
+        cache_handler = CacheHandler()
 
-    def do_POST(self) -> None:
-        self.middleware.handle(self)
+        dummy_handler.then(auth_handler)
+        auth_handler.then(cache_handler)
+        cache_handler.then(router)
+
+        return dummy_handler
+
+    def handler(self) -> CGIHTTPRequestHandler:
+        class Handler(CGIHTTPRequestHandler):
+            app = self
+
+            def do_GET(self) -> None:
+                self.app.middleware.handle(self)
+
+            def do_POST(self) -> None:
+                self.app.middleware.handle(self)
+
+        return Handler
 
 
 def main():
-    with TCPServer(("", 8080), Handler) as httpd:
+    app = Application()
+    with TCPServer(("", 8080), app.handler()) as httpd:
         print("Serving at port 8080")
         httpd.serve_forever()
 
